6434f5697d963797f31e841efe9a6dcb5dd7463f
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-10-03 01:24:25 +0000

    Oct 3, 2023, 1:24 AM

--- hello/hello.py
+++ hello/hello.py
@@ -1,2 +1,7 @@
-for i in range(5):
-    print("Hello")
+def main():
+    for i in range(4):
+        name = input("What's your name? ")
+        print(f"Hello {name}")
+
+if __name__ == "__main__":
+    main()
