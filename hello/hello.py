685c252441a24ebbd00dfd2ea5f12024383ba31d
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-10-25 03:19:36 +0000

    Oct 25, 2023, 3:19 AM

--- hello/hello.py
+++ hello/hello.py
@@ -1,7 +1,16 @@
 def main():
-    """
-    prints Hello on terminal
-    """
-    print("Helllo")
+    temp_celsius = float(input("Enter temperature in Celsius: "))
+    conversion_choice = input("Convert to Fahrenheit or Kelvin (F/K): ")
 
-main()
+    converted_temp = convert_temperature(temp_celsius, conversion_choice)
+    print(f"Converted temperature: {converted_temp:.2f}")
+
+def convert_temperature(temp_celsius, conversion_choice):
+    match conversion_choice:
+        case 'F':
+            return temp_celsius * 1.8 + 32
+        case 'K':
+            return temp_celsius + 273.15
+
+if __name__ == '__main__':
+    main()
