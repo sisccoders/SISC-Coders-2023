ce216af3583b3f7e4166ed6bbeb99e47f453c8fc
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-07 14:26:32 +0000

    Nov 7, 2023, 2:26 PM

--- Week 1/match_case_advanced.py
+++ Week 1/match_case_advanced.py
@@ -16,7 +16,6 @@ class Game:
             self.parse_command(command)
 
     def parse_command(self, command):
-        #TODO
         """
         Parses the given command from the user and executes the game action.
 
@@ -58,7 +57,44 @@ class Game:
         https://www.geeksforgeeks.org/python-exit-commands-quit-exit-sys-exit-and-os-_exit/
         - The wildcard '_' case is used as a fallback for unrecognized commands, providing an error message to the user.
 
-        This is advanced and is meant to be very hard and challenging. Don't feel afraid to collaborate with your friends or ask the leaders.
+
+        How to Test
+        1. Test Movement Commands:
+            - Use "go" followed by a direction (e.g., "go north") to move between rooms.
+            - Verify that you can only move in valid directions as per the current room's description.
+            - Check that attempting to move in an invalid direction provides the correct feedback.
+
+        2. Test Item Interaction:
+            - Use "pick up" followed by an item name (e.g., "pick up key") when in the correct room.
+            - Confirm that the item is added to the inventory and is no longer in the room.
+            - Attempt to pick up an item that isn't in the room or doesn't exist and check for appropriate feedback.
+
+        3. Test Character Interaction:
+            - Use "talk to" followed by a character name (e.g., "talk to guard") when in the same room as the character.
+            - Ensure that the character interaction is consistent with the game's story and the player's inventory state.
+            - Try talking to a character not present in the room and verify that the game responds correctly.
+
+        4. Test Quitting the Game:
+            - Enter "quit" and confirm that the game ends and exits as expected.
+            - Ensure that any end-of-game messages or save functionality works correctly.
+
+        5. Test Unrecognized Commands:
+            - Enter commands that are not supported by the game and verify that you receive a message indicating the command was not understood.
+
+        6. Test Game Progression:
+            - Play through the game as intended, using a combination of the above commands to progress.
+            - Verify that the game's state progresses and changes in response to your actions.
+
+        7. Test Error Handling and Edge Cases:
+            - Try inputs with extra spaces, mixed case, or unexpected order (e.g., "go  east", "PICK UP  KEY").
+            - Check how the game handles non-string inputs or special characters if applicable.
+
+        8. Test Game Save and Load Functionality (if implemented):
+            - If the game includes save and load features, test saving the game's state and then loading it.
+            - Ensure the game state is preserved accurately across save and load cycles.
+
+        This is advanced and is meant to give you an idea on what your future projects may look like.
+        It will be very hard and challenging. Don't feel afraid to collaborate with your friends or ask the leaders.
         """
         #TODO
 
