45932984413c2bcff22d465ba1ae635d9059357e
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 07:54:45 +0000

    Nov 9, 2023, 7:54 AM

--- Week 1/grades.py
+++ Week 1/grades.py
@@ -7,11 +7,11 @@ def convert_to_letter(percent_grade):
     else:
         if percent_grade > 95:
             print('VS')
-        if percent_grade > 90:
+        elif percent_grade > 90:
             print('S')
-        if percent_grade > 85:
+        elif percent_grade > 85:
             print('AA')
-        if percent_grade >= 80:
+        elif percent_grade >= 80:
             print('A')
 
 convert_to_letter(96)
