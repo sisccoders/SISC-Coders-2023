945ab08472c0b91a249dcb64e9ef9f656dc098a0
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 08:10:30 +0000

    Nov 9, 2023, 8:10 AM

--- Week 1/if_else_basic.py
+++ Week 1/if_else_basic.py
@@ -15,6 +15,13 @@ def categorize_age(age):
     Hint
     use if-else statement
     '''
-
+    if age >= 65:
+        print('Senior')
+    elif age > 20:
+        print('Adult')
+    elif age > 13:
+        print('Teen')
+    elif age >= 0:
+        print('Child')
 
 main()
