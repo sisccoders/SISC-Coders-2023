4cc336ed0927a8c42e86af9a5776fbda9345f169
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 03:58:46 +0000

    Nov 9, 2023, 3:58 AM

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,2 +1,20 @@
-grade = input('What is your grade? ')
-print(f'{grade}')
+def main():
+    numeric_grade = int(input("What's your grade? "))
+    letter_grade = convert_to_letter(numeric_grade)
+    print(letter_grade)
+
+def convert_to_letter(grade):
+    if grade > 95:
+        return 'VS'
+    elif grade > 90:
+        return 'S'
+    elif grade > 85:
+        return 'AA'
+    elif grade > 80:
+        return 'A'
+    elif grade > 70:
+        return 'LA'
+    else:
+        return 'P'
+
+main()
