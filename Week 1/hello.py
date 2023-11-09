627d3ad6c7e27b75c8d6d0a7519fad6c91ece046
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 06:37:04 +0000

    Second meeting

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,17 +1,2 @@
-def main():
-    percent_grade = float(input("What's your grade? "))
-    letter_grade = convert_to_letter(percent_grade)
-    print(letter_grade)
-
-def convert_to_letter(grade):
-    '''
-    Converts percent grade to letter grade
-
-    Parameters:
-    - grade: The numeric grade of the person.
-
-    Returns:
-    - str: The converted letter grade.
-    '''
-
-main()
+name = input('What is your name? ')
+print(f'Hello {name}')
