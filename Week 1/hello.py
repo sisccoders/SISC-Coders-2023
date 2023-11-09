d92a0488e55eda589cf1e2babaeafe06c42a1944
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 05:05:47 +0000

    Nov 9, 2023, 5:05 AM

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,9 +1,18 @@
 def main():
-    numeric_grade = int(input("What's your grade? "))
+    numeric_grade = float(input("What's your grade? "))
     letter_grade = convert_to_letter(numeric_grade)
     print(letter_grade)
 
 def convert_to_letter(grade):
+    '''
+    Converts numeric grade to letter grade
+
+    Parameters:
+    - grade: The numeric grade of the person.
+
+    Returns:
+    - str: The converted letter grade.
+    '''
     if grade > 95 and grade <= 100:
         return 'VS'
     elif grade > 90:
