07b9c2e6f7d403a8fe89eac83ea4892d065d7d67
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 05:07:40 +0000

    Nov 9, 2023, 5:07 AM

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -13,20 +13,5 @@ def convert_to_letter(grade):
     Returns:
     - str: The converted letter grade.
     '''
-    if grade <= 100:
-        if grade > 95:
-            return 'VS'
-        elif grade > 90:
-            return 'S'
-        elif grade > 85:
-            return 'AA'
-        elif grade >= 80:
-            return 'A'
-        elif grade >= 70:
-            return 'LA'
-        elif grade >= 0 and grade < 70:
-            return 'P'
-    else:
-        return 'Please type a valid grade (0-100).'
 
 main()
