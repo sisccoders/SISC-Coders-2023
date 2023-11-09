ce6071f9244c0e2dd3b0f2d5e51976f3ec04e99a
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 03:32:42 +0000

    Nov 9, 2023, 3:32 AM

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,26 +1,2 @@
-def main():
-    name = ask_name()
-    print(hello(name))
-
-def hello(name):
-    '''
-    Prints Hello followed by the name of a person.
-
-    Parameters:
-    - name (str): A string representing the name of the person.
-
-    Returns:
-    - str: A string message containing Hello name.
-    '''
-    return f'Hello {name}'
-
-def ask_name():
-    '''
-    Asks user for their name.
-
-    Returns:
-    - str: The input of the user.
-    '''
-    return input('What is your name? ')
-
-main()
+name = input('What is your name? ')
+print(f'Hello {name}')
