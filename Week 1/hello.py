f7dde9ad9311d62e310dd4072397188a1c7033a7
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 03:09:20 +0000

    Nov 9, 2023, 3:09 AM

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,5 +1,5 @@
 # To print Hello World on the console
 
-name = input('What is your name? ')
-print('Hello ', end='')
-print(name)
+def hello():
+    name = input('What is your name? ')
+    print(f'Hello {name}')
