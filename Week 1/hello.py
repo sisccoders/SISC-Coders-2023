5a4310c74e0be7aa84f47541d1544f1fedf1c66b
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 03:16:57 +0000

    Nov 9, 2023, 3:16 AM

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,8 +1,26 @@
-# To print Hello World on the console
-
 def main():
-    hello()
+    name = ask_name()
+    print(hello(name))
 
-def hello():
-    name = input('What is your name? ')
-    print(f'Hello {name}')
+def hello(name):
+    '''
+    Prints Hello followed by the name of a person.
+
+    Parameters:
+    - name (str): A string representing the name of the person.
+
+    Returns:
+    - str: A string message containing Hello name.
+    '''
+    return f'Hello {name}'
+
+def ask_name():
+    '''
+    Asks user for their name.
+
+    Returns:
+    - str: The input of the user.
+    '''
+    return input('What is your name? ')
+
+main()
