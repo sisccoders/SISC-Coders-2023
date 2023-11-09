4bd5fa4224a80bbbd35a47d131fe5222f9fd0c77
hyunkyu0 <99479410+hyunkyu0@users.noreply.github.com>
2023-11-09 07:45:28 +0000

    Nov 9, 2023, 7:45 AM

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,2 +1,15 @@
-name = input('What is your name? ')
-print(f'Hello {name}')
+def main():
+    name = receive_input()
+    hello(name)
+
+def receive_input():
+    name = input('What is your name? ')
+    return name
+
+def hello():
+    '''
+    Asks for the user's name and prints it in terminal
+    '''
+    print(f'Hello {name}')
+
+main()
