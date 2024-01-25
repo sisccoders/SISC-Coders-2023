44fe30b3f4bb079fe74d76b39c77846c64f298c3
Hyunkyu Song <hyunkyux0@gmail.com>
2024-01-25 08:13:57 +0000

    Jan 25, 2024, 8:13 AM

--- tmp2.py
+++ tmp2.py
@@ -2,8 +2,8 @@ import random
 
 z = random.choice(["Head","Tails"])
 
-user = input("Pick a Side, Head or Tails?: ")
 while True:
+    user = input("Pick a Side, Head or Tails?: ")
     if user != 'Head' and user != 'Tails':
         print('Please type Head or Tails')
     else:
