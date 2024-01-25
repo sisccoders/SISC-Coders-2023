4dc2e5a0bb0c8cba6fd057ba0c9fc4766c9f9e22
Hyunkyu Song <hyunkyux0@gmail.com>
2024-01-25 08:08:39 +0000

    Jan 25, 2024, 8:08 AM

--- tmp2.py
+++ tmp2.py
@@ -1,4 +1,6 @@
-from random import random
+import random
 
 #Create a code that simulates a fair coin toss:
 #it's going to print out Heads for a 50% chance and Tails for 50% chance
+
+random.random()
